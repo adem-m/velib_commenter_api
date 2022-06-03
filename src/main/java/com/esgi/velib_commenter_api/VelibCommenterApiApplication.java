80b3e79afc5ea4d4bcaac2d0c6f71281f7eec5cf
Adem Mrizak <adem.mrizak@gmail.com>
2022-06-03 08:22:47 +0200

    Initial commit

---
+++ src/main/java/com/esgi/velib_commenter_api/VelibCommenterApiApplication.java
@@ -0,0 +1,13 @@
+package com.esgi.velib_commenter_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VelibCommenterApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VelibCommenterApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/esgi/velib_commenter_api/VelibCommenterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esgi.velib_commenter_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VelibCommenterApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
